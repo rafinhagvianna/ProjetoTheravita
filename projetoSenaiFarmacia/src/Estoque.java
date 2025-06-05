a4f55767815f197fc9cf7b33759fb4586cc6b4d9
Gabriel <bachegagabriel@gmail.com>
2025-06-05 11:28:28 -0300

    produto e estoque atualizados

--- projetoSenaiFarmacia/src/Estoque.java
+++ projetoSenaiFarmacia/src/Estoque.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estoque {
+    private static List<Estoque> catalogo = new ArrayList<>();
     private Produto produto;
     private int estoque;
     private int minimo;
@@ -11,6 +15,7 @@ public class Estoque {
         this.estoque = estoque;
         this.minimo = minimo;
         this.produto = produto;
+        catalogo.add(this);
     }
 
     public Produto getProduto() {
@@ -37,11 +42,24 @@ public class Estoque {
         this.minimo = minimo;
     }
 
-    public int verificarMinimo() {
-        return 0;
+    public boolean verificarEstoque() { // Para o método verificarVenda na classe Venda
+        if (this.estoque < this.minimo) {
+            System.out.println("ATENÇÃO: O estoque de " + this.produto.getDescricao() + " está em " + this.estoque + " unidades, o que é abaixo do mínimo de " + this.minimo + ". Necessário nova compra.");
+            return false;
+        } else {
+            System.out.println("O estoque de " + this.produto.getDescricao() + " está OK (" + this.estoque + " unidades). Não é necessário nova compra.");
+            return true;
+        }
     }
-    
+
+    public static void getCatalogo() {
+        for (Estoque estoque : catalogo) {
+            System.out.println(estoque.produto);
+        }
+    }
+
     public int verificarVenda(int qtd) {
         return 0;
     }
+
 }
