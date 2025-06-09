52c663948813be54ea4143b86459297d6f4b720a
LuizFernandoMoreiraDomenico <luizfmd21@gmail.com>
2025-06-09 10:05:28 -0300

    Organização em diretórios e inclusão de interfaces

---
+++ projetoSenaiFarmacia/src/Classes/Itens.java
@@ -0,0 +1,32 @@
+package Classes;
+public class Itens {
+    
+    private int quantidade;
+    private Produto produto;
+
+    public Itens(int quantidade, Produto produto){
+        this.quantidade = quantidade;
+        this.produto = produto;
+    }
+    
+    public int getQuantidade(){
+        return quantidade;
+    }
+    public Produto getProduto(){
+        return produto;
+    }
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+    public void setProduto(Produto produto){
+        this.produto = produto;
+    }
+
+    public double valorVenda(){
+        return quantidade * produto.getValorVenda();
+    }
+    
+    public double valorCompra(){
+        return quantidade * produto.getValorCompra();
+    }
+}
---
+++ projetoSenaiFarmacia/src/Classes/Venda.java
@@ -0,0 +1,62 @@
+package Classes;
+import Enums.Status;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Venda extends Transacoes {
+    private Transportadora transportadora;
+
+    public Venda() {
+        super();
+    }
+
+    public Venda(double valor, LocalDate data) {
+        super(new ArrayList<Itens>(), data, null, null, valor);
+    }
+
+    public Venda(ArrayList<Itens> itens, LocalDate data, Funcionario funcionario, Status status, double valor) {
+        super( itens, data, funcionario, status, valor);
+    }
+
+    public Venda(Transportadora transportadora) {
+        this.transportadora = transportadora;
+    }
+
+    public Transportadora getTransportadora() {
+        return transportadora;
+    }
+
+    public void setTransportadora(Transportadora transportadora) {
+        this.transportadora = transportadora;
+    }
+
+    // Adiciona um item à venda
+    public void adicionarItem(Itens item) {
+        getProdutos().add(item);
+    }
+
+    // Remove um item da venda
+    public void removerItem(Itens item) {
+        getProdutos().remove(item);
+    }
+
+    // Calcula o total da venda
+    public double calculaTotal() {
+        double total = 0;
+        for (Itens item : getProdutos()) {
+            total += item.valorVenda();
+        }
+        total += transportadora.getTaxa() * total;
+        return total;
+    }
+
+    public static void emAberto(ArrayList<Venda> vendas) {
+        for (Venda venda : vendas) {
+            if (venda.getStatus().equals(Status.ABERTO)) {
+                System.out.println("Venda em aberto: " + venda.getId());
+            }
+        }
+    }
+}
+
